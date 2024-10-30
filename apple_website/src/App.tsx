0b61fef8d02356f41626a59770a2d0ceb4a31ee9
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2024-10-29 22:43:43 -0400

    more setup

--- apple_website/src/App.tsx
+++ apple_website/src/App.tsx
@@ -1,11 +1,14 @@
 import { ReactNode } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Highlights from './components/Highlight'
 
 const App = ():ReactNode=> {
   return (
     <main className='bg-black'>
-      Navbar
-      Hero
-      Highlights
+      <Navbar />
+      <Hero />
+      <Highlights />
     </main>
   )
 }

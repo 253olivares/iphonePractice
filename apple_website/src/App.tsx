94b17f42a3c1c7c2c88abe588bc502d32e9aace8
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2024-10-29 22:39:16 -0400

    setting up project

---
+++ apple_website/src/App.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react'
+
+const App = ():ReactNode=> {
+  return (
+    <main className='bg-black'>
+      Navbar
+      Hero
+      Highlights
+    </main>
+  )
+}
+
+export default App
